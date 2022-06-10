27758a687b7e1bcd206a847549721a07e6b4525b
gladiusglad <defoverthought@gmail.com>
2022-06-10 16:40:35 +0700

    Remove unused test constant

--- pages/v/[lid].tsx
+++ pages/v/[lid].tsx
@@ -30,7 +30,6 @@ const Viewer: NextPage = () => {
       {id: 2, text: 'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio.'}
     ]
   }
-  const pages = [0, 1, 2]
 
   const title = 'My Only One'
 
