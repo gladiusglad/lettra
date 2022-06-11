a6343e2e42fbe33a8dbe6d28767d1ac8892e1149
gladiusglad <defoverthought@gmail.com>
2022-06-11 23:28:18 +0700

    Add homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,58 +1,71 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import s from '../styles/Home.module.css'
+import background from '../img/desk.webp'
+import pen from '../img/pen.webp'
+import envelope from '../img/envelope.webp'
+import tag from '../img/tag.webp'
+import { useState } from 'react'
+import { motion } from 'framer-motion'
+import { useRouter } from "next/router";
 
 const Home: NextPage = () => {
+  const router = useRouter()
+  const [hoveredButton, setHoveredButton] = useState(0)
+  const [codeValue, setCodeValue] = useState('')
+
+  const onCodeSubmit = (e) => {
+    e.preventDefault()
+    router.push('/v/' + codeValue)
+  }
+
   return (
-    <div className={styles.container}>
+    <div className={s.container}>
       <Head>
         <title>Lettra</title>
         <meta name="description" content="Create and send an e-letter for your friends and loved ones!" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
+      <div className={s.background}>
+        <Image src={background} layout="fill" objectFit="cover" />
+      </div>
+      <div className={s.homeWrapper}>
+        <h1 className={s.title}>
           Lettra
         </h1>
-
-        <p className={styles.description}>
+        <p className={s.description}>
           Create and send an e-letter.
         </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        <div className={s.panelsWrapper}>
+          <button className={s.writeButton} onMouseEnter={() => setHoveredButton(-1)} onMouseLeave={() => setHoveredButton(0)}>
+            <div className={s.panelBg}>
+              <Image src={envelope} />
+            </div>
+            <p>Write letter</p>
+          </button>
+          <motion.div animate={{ rotate: hoveredButton * -10, transition: { ease: 'easeOut' } }}>
+            <Image src={pen} width={50} height={500} />
+          </motion.div>
+          <div className={s.codeTag} onMouseEnter={() => setHoveredButton(1)} onMouseLeave={() => setHoveredButton(0)}>
+            <div className={s.panelBg}>
+              <Image src={tag} />
+            </div>
+            <form onSubmit={onCodeSubmit}>
+              <label>
+                Enter code
+              </label>
+              <div className={s.codeInputs}>
+                <input className={s.codeInput} maxLength={6} value={codeValue} onChange={(e) => setCodeValue(e.target.value)} />
+                <input type={'submit'} className={s.codeSubmit} value="⮞" />
+              </div>
+            </form>
+          </div>
         </div>
-      </main>
-
-      <footer className={styles.footer}>
-        By GladGladius
+      </div>
+      <footer>
+        <span>Images by Unsplash and Pixabay</span>
+        <span>Made by GladGladius © 2022</span>
       </footer>
     </div>
   )
